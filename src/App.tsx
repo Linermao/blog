77762a989e8c0950e80643337bdba02844c3aed2
Linermao <LinermaoGemail@gmail.com>
2025-02-16 12:59:22 +0800

    Initial

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import react from './assets/react.svg'
+
+function App() {
+  return (
+    <>
+      <div className='w-10 h-10 bg-black'>
+        <img src={react} alt="" />
+      </div>
+    </>
+  )
+}
+
+export default App
